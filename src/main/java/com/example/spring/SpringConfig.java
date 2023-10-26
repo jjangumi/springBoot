7893aab8416388bdb153687515337348bbb6e2a8
eunmi <apricityrr@naver.com>
2023-10-26 21:19:03 +0900

    순수 jdbc를 이용한 데이터베이스 연결

--- src/main/java/com/example/spring/SpringConfig.java
+++ src/main/java/com/example/spring/SpringConfig.java
@@ -1,21 +1,35 @@
 package com.example.spring;
 
+import com.example.spring.repository.JdbcMemberRepository;
 import com.example.spring.repository.MemberRepository;
 import com.example.spring.repository.MemoryMemberRepository;
 import com.example.spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
+
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository(); 원래 메모리에 저장해놨던걸 구현체만 바꿔 설정해 데이터베이스에 저장한다.
+        return new JdbcMemberRepository(dataSource);
     }
 }
